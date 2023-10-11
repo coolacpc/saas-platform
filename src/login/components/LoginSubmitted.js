d22c07a0edebeee81a1330e1d0e55d4a3ff92536
coolacpc <coolacpc@gmail.com>
2023-10-11 09:19:58 +0300

    fixed unescaped entity 2 for vercel deployment

--- src/login/components/LoginSubmitted.js
+++ src/login/components/LoginSubmitted.js
@@ -4,8 +4,8 @@ export default function LoginSubmitted({ submitted }) {
 			<h1>Link Sent</h1>
 			<p>
 				Check your email <strong>{submitted}</strong> for the login
-				link. You can also check your spam folder if you don't see it in
-				your inbox.
+				link. You can also check your spam folder if you don&apos;t see
+				it in your inbox.
 			</p>
 		</div>
 	);
