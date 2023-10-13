0be024c5f7f95e403bc1cb1bd018f2eedb677603
coolacpc <coolacpc@gmail.com>
2023-10-13 19:38:16 +0300

    changed prod-url to vercel link

--- src/core/utils/index.js
+++ src/core/utils/index.js
@@ -1,3 +1,5 @@
 const production = process.env.NODE_ENV === "production";
 
-export const SITE_URL = production ? "prod-url" : "http://localhost:3000";
+export const SITE_URL = production
+	? "https://saas-platform-delta.vercel.app/"
+	: "http://localhost:3000";
